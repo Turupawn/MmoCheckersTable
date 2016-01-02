aaf6db754a57f202a9942757be8fb58dd7350ad7
Turupawn <ahmed.hn.43@gmail.com>
2016-01-02 10:39:33 -0600

    amarrando piezas

--- public/js/ClientSharedObject.js
+++ public/js/ClientSharedObject.js
@@ -24,10 +24,25 @@ function onDragStop(sprite, pointer) {
   result = sprite.key + " dropped at x:" + pointer.x + " y: " + pointer.y;
   var new_position_x = pointer.x-800/2-this.drag_adjustment_x//-800/2+game.camera.x
   var new_position_y = pointer.y-600/2-this.drag_adjustment_y//-600/2+game.camera.y
+  console.log("fds"+new_position_y)
+  if(new_position_x<-400)
+  {
+    console.log("fdsa")
+    new_position_x=-400
+  }
+  if(new_position_y<-300)
+    new_position_y=-300
+  if(new_position_x>350)
+    new_position_x=350
+  if(new_position_y>250)
+    new_position_y=250
+
+  this.my_sprite.x = new_position_x
+  this.my_sprite.y = new_position_y
+
   socket.emit('move object', { index: this.index, x: new_position_x, y: new_position_y })
   console.log('Client sent message: move object')
-  console.log(this.index)
-  console.log(this.drag_adjustment_x)
+
 }
 
 window.ClientSharedObject = ClientSharedObject
